295cb059b12511eebd4002e28af730b1de0315e7
VladMakar <vlad@cratustech.com>
2019-10-25 16:31:55 -0700

    fixed cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening at ${port}`));
 // app.use(express.static('public'));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
